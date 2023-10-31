ea569c347a923469e31826fc9692203f67b13ac0
nimba005 <nimbaoyugi@gmail.com>
2023-10-31 15:33:04 +0300

    updates

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,18 +8,18 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
-	if (parent == NULL)
-		return (NULL);
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = parent->right;
-	parent->right = new;
-	if (new->right)
-		new->right->parent = new;
-	return (new);
+binary_tree_t *new;
+if (parent == NULL)
+	return (NULL);
+new = malloc(sizeof(binary_tree_t));
+if (new == NULL)
+	return (NULL);
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = parent->right;
+parent->right = new;
+if (new->right)
+	new->right->parent = new;
+return (new);
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,9 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (!node->left && !node->right)
-		return (1);
+if (node == NULL)
 	return (0);
+if (!node->left && !node->right)
+	return (1);
+return (0);
 }
